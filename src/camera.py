d508bea1a1ca74afc94b15d0b53bb20abc1d9283
Richard Zhang <rich.zhang65@gmail.com>
2024-09-02 19:44:30 -0400

    Move to face detector class

--- src/camera.py
+++ src/camera.py
@@ -1,13 +1,8 @@
 import cv2
-from utils import get_circle_dim
 
 class Camera:
-    def __init__(self, margin=10, cascade='res/cascades/pretrained.xml'):
+    def __init__(self):
         self.vid = cv2.VideoCapture(0)
-
-        self.margin = margin
-
-        self.cascade = cv2.CascadeClassifier(cascade)
         
     def get_frame(self):
         ret, frame = self.vid.read()
@@ -20,14 +15,6 @@ class Camera:
         cv2.destroyAllWindows()
 
     def show_frame(self, frame):
-        grayed = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = self.cascade.detectMultiScale(grayed, 1.3, 5)
-
-        for (x, y, w, h) in faces:
-            center, radius = get_circle_dim(x, y, w, h)
-            cv2.circle(frame, center, radius + self.margin, (255, 0, 0), 2)
-            # cv2.rectangle(frame, (x-self.margin, y-self.margin), (x+w+self.margin, y+h+self.margin), (255, 0, 0), 2)
-        
         cv2.imshow("Camera", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             return False
---
+++ src/face_detector.py
@@ -0,0 +1,22 @@
+import cv2
+
+from utils import get_circle_dim
+
+class FaceDetector:
+    def __init__(self, margin=10, cascade='res/cascades/pretrained.xml'):
+        self.margin = margin
+
+        self.cascade = cv2.CascadeClassifier(cascade)
+
+    def detect_face(self, frame):
+        grayed = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.cascade.detectMultiScale(grayed, 1.3, 5)
+
+        return faces
+    
+    def show_bounding_box(self, frame, faces):
+        for (x, y, w, h) in faces:
+            center, radius = get_circle_dim(x, y, w, h)
+            cv2.circle(frame, center, radius + self.margin, (255, 0, 0), 2)
+            # cv2.rectangle(frame, (x-self.margin, y-self.margin), (x+w+self.margin, y+h+self.margin), (255, 0, 0), 2)
+    
